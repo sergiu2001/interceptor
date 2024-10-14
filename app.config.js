4b046f6ae324a9ad977371c59c5e0695f0b549fa
sergiulini22 <sergiu.frasie2@gmail.com>
2024-10-14 13:08:12 +0300

    Remove google-services.json file

---
+++ app.config.js
@@ -0,0 +1,53 @@
+// app.config.js
+
+import 'dotenv/config';
+
+export default {
+    name: "Terminus: Codex",
+    slug: "interceptor",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "dark",
+    splash: {
+        image: "./assets/images/splash.png",
+        resizeMode: "cover",
+        backgroundColor: "#201b2c"
+    },
+    ios: {
+        supportsTablet: true,
+        runtimeVersion: {
+            policy: "appVersion"
+        }
+    },
+    android: {
+        adaptiveIcon: {
+            foregroundImage: "./assets/images/adaptive-icon.png",
+            backgroundColor: "#ffffff"
+        },
+        package: "com.invictus.terminus",
+        runtimeVersion: "1.0.0",
+        googleServicesFile: process.env.GOOGLE_SERVICES_JSON
+    },
+    web: {
+        bundler: "metro",
+        output: "static",
+        favicon: "./assets/images/favicon.png"
+    },
+    experiments: {
+        typedRoutes: true
+    },
+    extra: {
+        router: {
+            origin: false
+        },
+        eas: {
+            projectId: "77c9af83-3532-44f5-93d8-58dce67112e2"
+        }
+    },
+    updates: {
+        url: "https://u.expo.dev/77c9af83-3532-44f5-93d8-58dce67112e2"
+    },
+    plugins: []
+};
